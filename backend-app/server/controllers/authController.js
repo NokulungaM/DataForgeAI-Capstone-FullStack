51392cb35859f46f3c9c6137a8c442065462ac8c
nhlanhla <roykeane888@gmail.com>
2024-09-26 12:39:46 +0200

    fixed line 35

--- backend-app/server/controllers/authController.js
+++ backend-app/server/controllers/authController.js
@@ -45,8 +45,8 @@ exports.signUp = async (req, res) => {
         const salt = await bcrypt.genSalt(10);
         const hashedPassword = await bcrypt.hash(password, salt);
         const user = new User({
-            email,
             username,
+            email,
             password: hashedPassword,
             ...rest
         });
