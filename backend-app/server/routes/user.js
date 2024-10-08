e3e49f47eae666b5e33ecd074b3ac22e57ced498
ImisebenziEmihle <ematyolo@gmail.com>
2024-10-08 09:22:26 +0200

    Things changed on this commit : 1. Added signup and sign in validation on the user and admin sign up and sign in routes, by calling the authMiddleware controller functions on the routes. 2. Uncommented the authmiddleware for the get users route for the admin.

--- backend-app/server/routes/user.js
+++ backend-app/server/routes/user.js
@@ -6,12 +6,13 @@ const {
     getUserIngredients } = require("../controllers/userController");
 const {  fetchAndDisplayRecipes } = require("../controllers/recipeController");
 const authMiddleware = require('../middleware/authMiddleware');
+const { validateSignUp , validateSignIn} = require('../controllers/authController');
 
 // Route to sign up a new user
-router.post('/signup', registerUser);
+router.post('/signup', validateSignUp , registerUser);
 
 // Route to log a user in (signin)
-router.post('/signin', loginUser);
+router.post('/signin', validateSignIn, loginUser);
 
 // Route to get user's ingredients 
 router.get('/ingredients', authMiddleware, getUserIngredients);
