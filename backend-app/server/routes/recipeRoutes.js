32cbf5783e6c8094e9695d6ed98a01159d1fcee4
ImisebenziEmihle <ematyolo@gmail.com>
2024-10-11 09:57:07 +0200

    Fixed some conflicts

--- backend-app/server/routes/recipeRoutes.js
+++ backend-app/server/routes/recipeRoutes.js
@@ -1,13 +1,8 @@
 const express = require('express');
 const router = express.Router();
-<<<<<<< HEAD
 const { fetchAndDisplayRecipes} = require('../controllers/recipeController');
 const authMiddleware = require('../middleware/authMiddleware');
 
-=======
-const { fetchAndDisplayRecipes } = require('../controllers/recipeController');
-const authMiddleware = require('../middleware/authMiddleware');
->>>>>>> 4691b9bd8e088ccf4f51b79fe40ecd3197b6aaa7
 
 // Single route for fetching recipes
 router.get('/recipes', authMiddleware, fetchAndDisplayRecipes);
