f80bee8d4805558373c7a0c5095dc2654f53d49d
Vuyo-Ngwane <ngwanevuyo7@gmail.com>
2024-10-14 14:45:33 +0200

    Added backend functionality for forgot password and reset passoword UI, updated dotenv file, updated authMiddleware, authController and signin page with link to forgotpassword

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,38 @@
+// src/App.js
+import React from 'react';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
+import Navbar from './components/Navbar'; // Import your components
+import Footer from './components/Footer';
+
+i18next
+  .use(initReactI18next)
+  .init({
+    lng: 'en', // Default language
+    fallbackLng: 'en', // Fallback language
+    resources: {
+      en: {
+        translation: require('./locales/en/translation.json'),
+      },
+      zh: {
+        translation: require('./locales/zh/translation.json'),
+      },
+    },
+  });
+
+const App = () => {
+  return (
+    <I18nextProvider i18n={i18next}>
+      <Navbar />
+      {/* Your other components */}
+      <Footer />
+    </I18nextProvider>
+  );
+};
+
+export default App;
+
 // import React from 'react';
 // import { Routes, Route } from 'react-router-dom';
 // import MainLayout from './layouts/MainLayout';
--- client/src/index.js
+++ client/src/index.js
@@ -1,3 +1,34 @@
+// src/index.js
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import i18next from 'i18next';
+import { I18nextProvider } from 'react-i18next';
+import { initReactI18next } from 'react-i18next';
+
+i18next
+  .use(initReactI18next)
+  .init({
+    lng: 'en', // Default language
+    fallbackLng: 'en', // Fallback language
+    resources: {
+      en: {
+        translation: require('./locales/en/translation.json'),
+      },
+      zh: {
+        translation: require('./locales/zh/translation.json'),
+      },
+    },
+  });
+
+ReactDOM.render(
+  <I18nextProvider i18n={i18next}>
+    <App />
+  </I18nextProvider>,
+  document.getElementById('root')
+);
+
+
 // import React from 'react';
 // import ReactDOM from 'react-dom';
 // import { BrowserRouter } from 'react-router-dom';
