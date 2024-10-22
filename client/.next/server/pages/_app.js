fb425a45112f6475288256b0478a9b7af4da7ab7
mpilomthiyane97 <mpilomthiyane97@gmail.com>
2024-10-22 12:06:30 +0200

    updated the text-to-speech audio functionality

--- client/.next/server/pages/_app.js
+++ client/.next/server/pages/_app.js
@@ -177,7 +177,7 @@ module.exports = require("zlib");
 var __webpack_require__ = require("../webpack-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/@swc","vendor-chunks/react-icons"], () => (__webpack_exec__("./src/pages/_app.js")));
+var __webpack_exports__ = __webpack_require__.X(0, ["vendor-chunks/next","vendor-chunks/react-icons"], () => (__webpack_exec__("./src/pages/_app.js")));
 module.exports = __webpack_exports__;
 
 })();
--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "33e4477e799b7cdd"; }
+/******/ 		__webpack_require__.h = function() { return "4be86a6764b61971"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
