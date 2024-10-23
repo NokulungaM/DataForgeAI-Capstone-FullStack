b423d514636da9515e41c0af55a5d18703fb2e82
mpilomthiyane97 <mpilomthiyane97@gmail.com>
2024-10-23 15:14:01 +0200

    changed featured recipes into a slide

--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "6a2cb157f663d56d"; }
+/******/ 		__webpack_require__.h = function() { return "50f909b06ac57041"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
