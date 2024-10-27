f294bc0e96b67d81588bcb0e55af26e0302eaab4
nhlanhla <roykeane888@gmail.com>
2024-10-27 23:55:40 +0200

    dependancies

--- client/.next/static/development/_buildManifest.js
+++ client/.next/static/development/_buildManifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
-=======
-self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/communityPage":["static\u002Fchunks\u002Fpages\u002FcommunityPage.js"],sortedPages:["\u002F_app","\u002F_error","\u002FcommunityPage"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
->>>>>>> 3d785ce78ccb1471d84f36ca338843c1ecdf2e60
+self.__BUILD_MANIFEST = {__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static\u002Fchunks\u002Fpages\u002Findex.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error.js"],"/auth/signin":["static\u002Fchunks\u002Fpages\u002Fauth\u002Fsignin.js"],"/search":["static\u002Fchunks\u002Fpages\u002Fsearch.js"],sortedPages:["\u002F","\u002F_app","\u002F_error","\u002Fauth\u002Fsignin","\u002Fsearch"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
